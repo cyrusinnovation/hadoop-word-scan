37dc5d58763f6b302071e43240eaeea9a3a6e7e3
mattfowler <matt.fow@gmail.com>
2013-02-20 12:55:22 -0500

    Cleanup himplicts to be something more reasonable, use it in tests.

--- src/test/scala/com/app/WordCountMapTest.scala
+++ src/test/scala/com/app/WordCountMapTest.scala
@@ -1,26 +1,27 @@
 package com.app
 
-import org.scalatest.{FunSpec}
+import org.scalatest.FunSpec
+import org.apache.hadoop.mrunit.mapreduce.MapDriver
 
 import org.scalatest.matchers.ShouldMatchers
 import org.slf4j.LoggerFactory
-import org.apache.hadoop.io.{LongWritable, Text}
+import org.apache.hadoop.io.{IntWritable, LongWritable, Text}
 import org.apache.hadoop.mapreduce.Mapper
 
-class WordCountMapTest extends FunSpec with ShouldMatchers {
+class WordCountMapTest extends FunSpec with ShouldMatchers with HadoopImplicitConversions{
   val logger = LoggerFactory.getLogger(classOf[WordCountMapTest])
 
   describe ("counting words") {
     it("should do some shit") {
-      val mapper: Mapper[LongWritable, Text, Text, Text] = new LogScanMapper
+      val mapper: Mapper[LongWritable, Text, Text, IntWritable] = new WordCountMap
 
-      val mapDriver = new MapDriver[LongWritable, Text, Text, Text]()
+      val mapDriver = new MapDriver[LongWritable, Text, Text, IntWritable]()
       mapDriver.getConfiguration.setQuietMode(false)
       mapDriver.setMapper(mapper)
 
       mapDriver.withInput(1, "world Hello World")
-      mapDriver.withOutput("world", "a")
-      val result = mapDriver.runTest()
+      mapDriver.withOutput("world", 1)
+      mapDriver.runTest()
     }
   }
 }
