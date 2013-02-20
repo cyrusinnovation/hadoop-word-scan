8114893aaf51a8789fcafa0b019028aeae6eaf73
mattfowler <matt.fow@gmail.com>
2013-02-20 13:29:21 -0500

    Clean up to use before and after setup trait

--- src/test/scala/com/app/WordCountMapTest.scala
+++ src/test/scala/com/app/WordCountMapTest.scala
@@ -1,34 +1,34 @@
 package com.app
 
-import org.scalatest.FunSpec
+import org.scalatest.{BeforeAndAfter, FunSpec}
 import org.apache.hadoop.mrunit.mapreduce.MapDriver
 
 import org.scalatest.matchers.ShouldMatchers
 import org.slf4j.LoggerFactory
 import org.apache.hadoop.io.{IntWritable, LongWritable, Text}
-import org.apache.hadoop.mapreduce.Mapper
 
-class WordCountMapTest extends FunSpec with ShouldMatchers with HadoopImplicitConversions{
+class WordCountMapTest extends FunSpec with ShouldMatchers with HadoopImplicitConversions with BeforeAndAfter {
   val logger = LoggerFactory.getLogger(classOf[WordCountMapTest])
+  val mapper = new WordCountMap
+  val mapDriver = new MapDriver[LongWritable, Text, Text, IntWritable]()
+
+  before {
+    mapDriver.getConfiguration.setQuietMode(false)
+    mapDriver.setMapper(mapper)
+  }
+
+  after {
+    mapDriver.resetOutput()
+  }
 
   describe ("counting words") {
     it("should be able to count the occurences of all words in an input") {
-      val mapper: Mapper[LongWritable, Text, Text, IntWritable] = new WordCountMap
-      val mapDriver = new MapDriver[LongWritable, Text, Text, IntWritable]()
-      mapDriver.getConfiguration.setQuietMode(false)
-      mapDriver.setMapper(mapper)
-
       mapDriver.withInput(1, "testing testing one one testing false")
       mapDriver.withOutput("false", 1).withOutput("testing", 3).withOutput("one", 2)
       mapDriver.runTest()
     }
 
     it("should be able to handle an empty input") {
-      val mapper: Mapper[LongWritable, Text, Text, IntWritable] = new WordCountMap
-      val mapDriver = new MapDriver[LongWritable, Text, Text, IntWritable]()
-      mapDriver.getConfiguration.setQuietMode(false)
-      mapDriver.setMapper(mapper)
-
       mapDriver.withInput(1, "")
       mapDriver.runTest()
     }
